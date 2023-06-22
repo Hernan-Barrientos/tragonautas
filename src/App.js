4699e23a57c529b4870f18235a0f050f89db861e
Hernan-Barrientos <hmb1989@gmail.com>
2023-06-22 19:04:21 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/tragonautasLogo.png'
+import galaxyBackground from "./images/galaxyCloud.png";
+import ig from "./images/ig.webp";
+import whatsapp from "./images/whatsapp-logo-png-0.png";
+import tragoImage from "./images/trago1.webp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='mainTragonautas'>
+      <div className='bannerTragonautas'>
+        <div className='instagram'>
+        <img src={ig} alt="ig" className='insta'></img>
+        </div>
+        <div className='containerLogoTragonautas'>
+          <img src={logo} alt="logo" className='logoTragonautas'></img>
+        </div>
+        <div className='whatsapp'>
+        <img src={whatsapp} alt="ws" className='wsapp'></img>
+        </div>
+      </div>
+      <div className='boddyTragonautas'>
+        <div className='tragosContainer'>Nombre del trago</div>
+        <div className='tragosContainer'>Nombre del trago</div>
+        <div className='tragosContainer'>Nombre del trago</div>
+        <div className='tragosContainer'>Nombre del trago</div>
+        <div className='tragosContainer'>Nombre del trago</div>
+        <div className='tragosContainer'>Nombre del trago</div>
+        <div className='tragosContainer'>Nombre del trago</div>
+        <div className='tragosContainer'>Nombre del trago</div>
+        <div className='tragosContainer'>Nombre del trago</div>
+      </div>
+    </div>)
 }
 
 export default App;
