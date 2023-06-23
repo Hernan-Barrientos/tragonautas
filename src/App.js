05e77c4d1bda23b4fe7ecc275b86fb3189688030
Hernan-Barrientos <hmb1989@gmail.com>
2023-06-23 19:16:13 -0300

    add

--- src/App.js
+++ src/App.js
@@ -4,31 +4,39 @@ import galaxyBackground from "./images/galaxyCloud.png";
 import ig from "./images/ig.webp";
 import whatsapp from "./images/whatsapp-logo-png-0.png";
 import tragoImage from "./images/trago1.webp";
+import trago2 from "./images/tragos/607224ece59b5e2e81fd.jpg";
+import trago3 from "./images/tragos/a4d50f754cd2cd6cb029.jpg";
 
 function App() {
   return (
     <div className='mainTragonautas'>
       <div className='bannerTragonautas'>
         <div className='instagram'>
-        <img src={ig} alt="ig" className='insta'></img>
+          <a href='https://www.instagram.com/tragonautas4.20/' target='blank'><img src={ig} alt="ig" className='insta'></img></a>
         </div>
         <div className='containerLogoTragonautas'>
           <img src={logo} alt="logo" className='logoTragonautas'></img>
         </div>
         <div className='whatsapp'>
-        <img src={whatsapp} alt="ws" className='wsapp'></img>
+          <a href='https://wa.me/541123880686' target='blank'><img src={whatsapp} alt="ws" className='wsapp'></img></a>
         </div>
       </div>
+      <div className='telefono'>HACE TU PEDIDO POR WHATSAPP AL 0303-456</div>
       <div className='boddyTragonautas'>
-        <div className='tragosContainer'>Nombre del trago</div>
-        <div className='tragosContainer'>Nombre del trago</div>
-        <div className='tragosContainer'>Nombre del trago</div>
-        <div className='tragosContainer'>Nombre del trago</div>
-        <div className='tragosContainer'>Nombre del trago</div>
-        <div className='tragosContainer'>Nombre del trago</div>
-        <div className='tragosContainer'>Nombre del trago</div>
-        <div className='tragosContainer'>Nombre del trago</div>
-        <div className='tragosContainer'>Nombre del trago</div>
+        <div className='centeringContainer'>
+          <div className='tragosContainer'><img src={tragoImage} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={trago2} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={trago3} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={tragoImage} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={trago2} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={trago3} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={tragoImage} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={trago2} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={trago3} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={tragoImage} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={trago2} alt="trago" className='fotoTragos'></img></div>
+          <div className='tragosContainer'><img src={trago3} alt="trago" className='fotoTragos'></img></div>
+        </div>
       </div>
     </div>)
 }
